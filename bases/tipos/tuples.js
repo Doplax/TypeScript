68f04283ad525659a1482515a57c1cb30e1f1314
Doplax <doplax@gmail.com>
2025-01-11 13:23:09 +0100

    feat: tuples

---
+++ bases/tipos/tuples.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+    Tuples allow us to control the data type of each item separately.
+*/
+(() => {
+    const hero = ['Dr Strange', 100, true];
+    //hero[0] = 1;
+    hero[0] = 'Iron Man';
+    hero[1] = 50;
+    hero[2] = false;
+})();
---
+++ bases/tipos/tuples.ts
@@ -0,0 +1,14 @@
+/*
+    Tuples allow us to control the data type of each item separately.
+*/
+(() => {
+
+    const hero: [string, number, boolean] = ['Dr Strange', 100, true];
+
+    //hero[0] = 1;
+    hero[0] = 'Iron Man';
+    hero[1] = 50;
+    hero[2] = false;
+
+
+})();
