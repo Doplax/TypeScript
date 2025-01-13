6f9d62fdaec58483128acb004dce81997cf447d1
Doplax <doplax@gmail.com>
2025-01-13 18:42:36 +0100

    feat: Change in ts configuration to export code to main.js

--- bases/funciones/args-default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-    const fullName = (firstName, lastName, upper = false) => {
-        // Si no pasamos un parametro, este será undefined, por eso usaremos || para asignar un valor
-        return `${firstName} ${lastName || 'No lastname'}`;
-    };
-    const name = fullName('Tony', 'stark', true);
-    console.log({ name });
-})();
--- bases/funciones/args-optional.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-    const fullName = (firstName, lastName) => {
-        // Si no pasamos un parametro, este será undefined, por eso usaremos || para asignar un valor
-        return `${firstName} ${lastName || 'No lastname'}`;
-    };
-    const name = fullName('Tony', true);
-    console.log({ name });
-})();
