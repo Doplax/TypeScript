300f09b025716f918e456ca53b273a3862a4fe58
Doplax <doplax@gmail.com>
2025-01-15 16:20:25 +0100

    feat: public and private methods

--- bases/classes/basic-short-way.ts
+++ bases/classes/basic-short-way.ts
@@ -1,17 +1,26 @@
+import { isBinaryOperatorToken } from "typescript";
+
 (() => {
   class Avenger {
     //private name;
     //private team;
     //public realName?: string;
     static avgAge: number = 35; // La propiedad estatica la podemos dejar fuera del constructor
+    static getAvgAge() {
+      return this.name + " " + this.avgAge;
+    }
 
     //Directamente en el constructor
     constructor(
       private name: string,
       private team: string,
-      avgAge: number = 55,
+      //avgAge: number = 55,
       public realName?: string,
     ) {}
+
+    bio() {
+      return `${this.name} (${this.team})`;
+    }
   }
 
   const antman: Avenger = new Avenger("Antman", "Capitan", "Scott Lang");
