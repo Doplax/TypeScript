db68678a3ca08e3dd2365ebd7cb029e024d45691
Doplax <doplax@gmail.com>
2025-01-15 19:49:52 +0100

    feat: basic interface

---
+++ bases/interfaces/basics.ts
@@ -0,0 +1,33 @@
+(() => {
+    //La única diferencia entre estos es que los tipos no son extensibles
+    type Hero = {
+    name: string;
+    age?: number;
+    powers: string[];
+    getName?: () => string;
+  };
+
+//  interface Hero  {
+//    name: string;
+//    age?: number;
+//    powers: string[];
+//    getName?: () => string;
+//  };
+
+  let flash: Hero = {
+    name: "Barry Allen",
+    age: 24,
+    powers: ["Super velocidad", "Viajar en el tiempo"],
+  };
+
+  let superman: Hero = {
+    name: "Clark Kent",
+    age: 60,
+    powers: ["Super fuerza"],
+    getName() {
+      return this.name;
+    },
+  };
+
+  })();
+  
