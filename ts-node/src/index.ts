3d2da42f6edbf5e024831925bbfef9ae35e598f9
Doplax <doplax@gmail.com>
2025-01-19 19:28:10 +0100

    feat: generic functions

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,6 +1,8 @@
 import { genericFunction } from "./generics/generics";
 
+
+const name: string = 'Doplax';
 // Nos está dejando usar metodos de string, numero ,ect cuando no es su tipo de dato
-console.log( genericFunction(3.1416).toFixed(2) );
-console.log( genericFunction('Hello World').toUpperCase() );
+console.log( genericFunction(3.1416));
+console.log( genericFunction(name).toUpperCase() );
 console.log( genericFunction(new Date()).getDate() );
