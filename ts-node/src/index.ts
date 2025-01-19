70088707565c1880a2afe799283bb666f7eef83d
Doplax <doplax@gmail.com>
2025-01-19 19:59:52 +0100

    feat: Example of a generic function in Action

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,17 @@
+import { Hero } from "./interfaces/hero";
 import { genericFunction } from "./generics/generics";
+import { Villain } from "./interfaces/villain";
 
-
-const name: string = 'Doplax';
 // Nos está dejando usar metodos de string, numero ,ect cuando no es su tipo de dato
-console.log( genericFunction(3.1416));
-console.log( genericFunction(name).toUpperCase() );
-console.log( genericFunction(new Date()).getDate() );
+//console.log( genericFunction(3.1416));
+//console.log( genericFunction(name).toUpperCase() );
+//console.log( genericFunction(new Date()).getDate() );
+
+
+const deadpol = {
+    name: 'Wade',
+    realName: 'Deadpool',
+    dangerLevel: 130
+}
+
+console.log( genericFunction<Villain>(deadpol) );
