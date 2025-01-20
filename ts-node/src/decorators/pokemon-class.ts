80aeea25420996065ecccd3eadd15aa05b564ad5
Doplax <doplax@gmail.com>
2025-01-20 19:00:59 +0100

    feat: Example of a decorator - Block Prototype

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -10,6 +10,14 @@ const printToConsoleConditional = (print: boolean = false): Function => {
     }
 }
 
+//Bloauqador para bloquear el prototipo de una clase
+const blockPrototype = function(constructor: Function){
+    Object.seal(constructor); //Prevents the modification of attributes of existing properties, and prevents the addition of new properties.
+    Object.seal(constructor.prototype);
+}
+
+@blockPrototype
+
 @printToConsoleConditional(false) // Llamamos al decorado
 export class Pokemon {
 
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -3,5 +3,9 @@ import { Pokemon } from "./decorators/pokemon-class";
 
 const charmander = new Pokemon('Charmander');
 
+//Pokemon.prototype.customName = 'Pickachu'
+(Pokemon.prototype as any).customName = 'Pickachu'// Para evitar problemas de tipado
+
+
 console.log(charmander);
 
